1a29f72d8056753e354d753f6dd714daa0a7bbbf
amuthan-rudimax <amuthan@rudimax.com>
2021-09-30 11:01:49 +0530

    Subscription Report

--- resources/views/subscription-report.blade.php
+++ resources/views/subscription-report.blade.php
@@ -1,28 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
-    <title>Subscription Report </title>
-    <link rel="icon" type="image/x-icon" href="assets/img/favicon.png"/>
-    <!-- BEGIN GLOBAL MANDATORY STYLES -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+@extends('layout.template')
+@push('css-links')
     <!-- Data Tables -->
     <link href="https://cdn.datatables.net/1.11.2/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
     <link href="https://cdn.datatables.net/buttons/2.0.0/css/buttons.dataTables.min.css" rel="stylesheet" type="text/css" />
     <!-- Data Tables -->
-
-    <!-- END GLOBAL MANDATORY STYLES -->
-
-
-</head>
-<body>
+@endpush
+@section('content')
 <div class="container">
-  <div class="row">
-  <div class="col-sm">
-  <br><br><br>
+<h6>Subscription Report</h6><hr>
 <table id="example" class="table table-striped table-bordered dt-responsive nowrap" style="width:100%">
 <thead>
     <tr>
@@ -140,17 +125,9 @@
     </tr>
     </tbody>
 </table>
-</div>
   </div>
-</div>
-  
-
-    <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
-    <!-- <script src="assets/js/libs/jquery-3.1.1.min.js"></script> -->
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <!-- <script src="bootstrap/js/popper.min.js"></script> -->
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-    <!-- <script src="plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script> -->
+@endsection
+@push('js-links')
     <!-- Data Tables -->
     <script src="https://cdn.datatables.net/1.11.2/js/jquery.dataTables.min.js"></script>
     <!-- <script src="https://cdn.datatables.net/1.11.2/js/dataTables.bootstrap4.min.js"></script> -->
@@ -163,19 +140,8 @@
     <script src="https://cdn.datatables.net/buttons/2.0.0/js/buttons.html5.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.0.0/js/buttons.print.min.js"></script>
     <!-- Data Tables -->
-    <script src="assets/js/app.js"></script>
-    <script>
-        $(document).ready(function() {
-            App.init();
-        });
-    </script>
-    <script src="assets/js/custom.js"></script>
-    <!-- END GLOBAL MANDATORY SCRIPTS -->
-
-    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
-    <script src="plugins/apex/apexcharts.min.js"></script>
-    <script src="assets/js/dashboard/dash_1.js"></script>
-    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+@endpush
+@section('js-content')
 <script>
 $(document).ready(function() {
     $('#example').DataTable( {
@@ -192,5 +158,4 @@ $(document).ready(function() {
     } );
 } );
 </script>
-</body>
-</html>
+@endsection
