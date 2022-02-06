28cfd7f288bab03f90d212bdbb15c50146676984
sanrudi <santhosh@rudimax.com>
2022-02-07 01:27:01 +0530

    navbar moved the usermail outside of the dropdown

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -24,12 +24,10 @@
                     <a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="userProfileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+                        {{ Auth::user()->email }}
                     </a>
                     <div class="dropdown-menu position-absolute" aria-labelledby="userProfileDropdown">
                         <div class="">
-                            <div class="dropdown-item">
-                            <a class="dropdown-item" href="#" >{{ Auth::user()->email }}</a>
-                            </div>
                             <div class="dropdown-item">
                                   <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
               document.getElementById('logout-form').submit();">
