b5766da22fe729db2cc9ef71cc0c496b4445d499
sanrudi <santhosh@rudimax.com>
2022-02-28 14:16:17 +0530

    changes in navbar dropdown

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -20,7 +20,7 @@
                     @endforeach
                 </a>
                 </li>
-                <li class="user-profile-dropdown  order-lg-0 order-1">
+                <li class="dropdown  user-profile-dropdown  order-lg-0 order-1">
                     <a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="userProfileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
